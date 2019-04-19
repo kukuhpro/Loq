c7b19926286cee72f167c306a138bfdd4fc7a140
Kukuh Prabowo <kukuh@qasir.id>
2019-04-19 18:45:13 +0700

    added for configuration read

--- loq-server/config.go
+++ loq-server/config.go
@@ -1,11 +1,43 @@
 package loqserver
 
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
 type Config struct {
 	ConfigurationPath string
 }
 
-func (c *Config) setupConfigurationVariable() {
+func (c *Config) Get(key string) string {
+	return os.Getenv(key)
+}
 
+func (c *Config) setValueConfiguration(array []string) bool {
+	for _, val := range array {
+		if !strings.Contains(val, "#") {
+			r, _ := regexp.Compile("(.*)=(.*)")
+			listString := r.FindStringSubmatch(val)
+			if len(listString) >= 3 {
+				os.Setenv(listString[1], listString[2])
+			}
+		}
+	}
+	return true
+}
+
+func (c *Config) setupConfigurationVariable() bool {
+	file, err := os.Open(c.ConfigurationPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	b, err := ioutil.ReadAll(file)
+	arraySplitString := strings.Split(string(b), "\n")
+	return c.setValueConfiguration(arraySplitString)
 }
 
 func NewConfig() *Config {
---
+++ loq-server/config_test.go
@@ -0,0 +1,37 @@
+package loqserver
+
+import "testing"
+
+type ConfigTestSuite struct {
+	config *Config
+}
+
+var configTestSuite *ConfigTestSuite
+
+func init() {
+	configTestSuite = &ConfigTestSuite{}
+	configTestSuite.config = NewConfig()
+}
+
+func TestSetupConfiguration(t *testing.T) {
+	config := configTestSuite.config
+	resultValue := config.setupConfigurationVariable()
+	if !resultValue {
+		t.Errorf("Result value must be True")
+	}
+}
+
+var tableDrivenGetConfiguration = map[string]string{
+	"produce_log":       "folder",
+	"path_folder_local": "log",
+}
+
+func TestGetConfiguration(t *testing.T) {
+	config := configTestSuite.config
+	for k, v := range tableDrivenGetConfiguration {
+		result := config.Get(k)
+		if result != v {
+			t.Errorf("You have actual result '" + result + "' and expected result '" + v + "'")
+		}
+	}
+}
